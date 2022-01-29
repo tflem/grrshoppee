656c4bd4f9dd04c17b39282f63030692039ace84
Tim Fleming <tfleming26@hotmail.com>
2022-01-28 18:29:56 -0600

    Fix mobile responsiveness and css map error

--- spec/system/creating_products_spec.rb
+++ spec/system/creating_products_spec.rb
@@ -2,9 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Our users can create new products", type: :system do
   scenario "with valid attributes" do
-    visit root_path
-
-    click_link "Add Products"
+    visit new_product_path
 
     fill_in "Name", with: "Chocolate Milk"
     fill_in "Quantity", with: 2
