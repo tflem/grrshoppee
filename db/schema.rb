865008687ebab4088e1ff006e929555675cbaa03
Tim Fleming <tfleming26@hotmail.com>
2022-02-20 15:41:39 -0600

    Setup new dev env and update rails and ruby versions

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_225858) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_17_225858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
