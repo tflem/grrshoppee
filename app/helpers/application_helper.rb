884b115d65220418562c1b77f6913e3f9c0acdfb
Tim Fleming <tfleming26@hotmail.com>
2022-03-14 19:54:01 -0600

    Show page now has product title functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_title(*items)
+    unless items.empty?
+      content_for :show_title do
+        (items << "Grr . . . Shoppee").join(" - ")
+      end
+    end
+  end
 end
--- spec/system/creating_products_spec.rb
+++ spec/system/creating_products_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe "Our users can create new products", type: :system do
     click_button "Create Product"
 
     expect(page).to have_content "Your product has been added."
+
+    product = Product.find_by!(name: "Chocolate Milk")
+    expect(page.current_path).to eq product_path(product)
+
+    show_title = "Chocolate Milk - Products - Grr . . . Shoppee"
+    expect(page).to have_title show_title
   end
 end
